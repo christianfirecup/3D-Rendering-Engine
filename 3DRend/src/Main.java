0876b392a116e52c7c9dad1e20a704f5564d0498
Christianfirecup <Christianfirecup@aol.com>
2026-02-17 10:28:17 -0500

    started shapes and vertexts

--- 3DRend/src/Main.java
+++ 3DRend/src/Main.java
@@ -1,7 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Triangle> tris = new ArrayList<>();
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
 
+
+
+
+
+
+
+            JFrame frame = new JFrame();
+            Container pane = frame.getContentPane();
+            pane.setLayout(new BorderLayout());
+            JPanel renderP = new JPanel(){
+                public void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.setColor(Color.BLACK);
+                    g2.fillRect(0, 0, getWidth(), getHeight());
+                    g2.translate(getWidth() / 2, getHeight() / 2);
+                    g2.setColor(Color.WHITE);
+                    for (Triangle t : tris) {
+                        Path2D path = new Path2D.Double();
+                        path.moveTo(t.v1.x, t.v1.y);
+                        path.lineTo(t.v2.x, t.v2.y);
+                        path.lineTo(t.v3.x, t.v3.y);
+                        path.closePath();
+                        g2.draw(path);
+                    }
+
+                }
+            };
+            pane.add(renderP, BorderLayout.CENTER);
+            frame.setSize(600,600);
+            frame.setVisible(true);
 
         }
 }
